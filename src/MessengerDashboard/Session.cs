b1dec60f436ade41223ab2dcbb2bd38141caec65
prathamnagpure <prathamnagpure2002@gmail.com>
2023-10-14 00:07:16 +0530

    created client and server folders

---
+++ src/MessengerDashboard/Session.cs
@@ -0,0 +1,18 @@
+﻿// ======================================
+// Author Information
+// --------------------------------------
+// Name        : Aradhya Bijalwan 
+// Roll Number : 112001006
+// ======================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerDashboard
+{
+    internal class Session
+    {
+    }
+}
--- src/MessengerDashboard/User.cs
+++ src/MessengerDashboard/User.cs
@@ -1,3 +1,9 @@
+// ======================================
+// Author Information
+// --------------------------------------
+// Name        : Shailab Chauhan 
+// Roll Number : 112001038
+// ======================================
 using System;
 
 namespace MessengerDashboard

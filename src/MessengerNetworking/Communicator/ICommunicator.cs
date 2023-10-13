38e9cdef4dfa55609c4a21f490886625ac2525b8
Vikas <112001049@smail.iitpkd.ac.in>
2023-10-13 07:50:15 +0530

    ICommunicator added

--- src/MessengerNetworking/Communicator/ICommunicator.cs
+++ src/MessengerNetworking/Communicator/ICommunicator.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MessengerNetworking.Communicator
 {
-    internal interface Interface1
+    public interface ICommunicator
     {
+        public void Send(string serializedData, string senderModule, string destination);
+
+        public void Broadcast(string serializedData, string senderModule);
+
+        public void AddClient(string client, TcpClient clientSocket);
+
+        public void RemoveClient(string client);
     }
 }

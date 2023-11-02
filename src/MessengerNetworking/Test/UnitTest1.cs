5dec34d395ea8b5b6b0e2d2c2e44da10c91576ce
priyanshu87511 <110776345+priyanshu87511@users.noreply.github.com>
2023-11-03 01:44:46 +0530

    two tests added

---
+++ src/MessengerNetworking/Test/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessengerNetworking.Communicator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessengerNetworking.Test
+{
+
+    /// <summary>
+    /// Checking Broadcast method of client
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string serverIp = "127.0.0.1";
+            int serverPort = 12345;
+            string messageToSend = "Hello, client!";
+
+            ServerCommunicator server = new ( serverPort );
+            server.Start();
+
+            ClientCommunicator client = new ( serverIp , serverPort );
+            client.Connect();
+
+
+            /// review what senderModule requires
+            client.Broadcast( messageToSend , serverIp );
+
+            Assert.AreEqual( messageToSend , server.latestMessage );
+
+            client.Disconnect();
+            server.Stop();
+        }
+    }
+}

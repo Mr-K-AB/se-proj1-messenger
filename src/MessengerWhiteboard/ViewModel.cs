a9e6ae54a33750b48d5b7dd7e3544e3aab63ab5f
Sanjh-Maheshwari <112001055@smail.iitpkd.ac.in>
2023-11-03 11:38:26 +0530

    shape customisation added

--- src/MessengerWhiteboard/ViewModel.cs
+++ src/MessengerWhiteboard/ViewModel.cs
@@ -14,8 +14,9 @@ namespace MessengerWhiteboard
         public ObservableCollection<ShapeItem> ShapeItems { get; set; }
         ShapeItem? tempShape;
 
-        public string userID = "user1";
+        private string userID = "tempUser";
 
+        public bool isEnabled;
         public enum WBModes
         {
             CreateMode,
@@ -25,13 +26,26 @@ namespace MessengerWhiteboard
         }
 
         private WBModes currentMode;
-
+        //shape attributes
+        Brush fillBrush;                                            // stores color of the object (fill colour)
+        Brush borderBrush;                                 // stores color of the border
+        int strokeWidth;  
+        
+        List<ShapeItem> selectedShapes;                                         // thickness of the stroke
 
         public ViewModel()
         {
             ShapeItems = new();
-            currentMode = WBModes.ViewMode;
-
+            this.currentMode = WBModes.ViewMode;
+            if(this.userID == "tempUser")
+            {
+                this.isEnabled = false;
+            }
+            SetUserID();
+            this.fillBrush = null;                                            // stores color of the object (fill colour)
+            this.borderBrush = Brushes.Black;                                 // stores color of the border
+            this.strokeWidth = 1;
+            this.selectedShapes = new List<ShapeItem>;
         }
 
         private static ViewModel? _instance;
@@ -64,5 +78,67 @@ namespace MessengerWhiteboard
             this.currentMode = mode; 
         }
 
+        public string GetUserID()
+        {
+            // return this.userID;
+            return "user1";
+        }
+
+        public void SetUserID()
+        {
+            this.userID = this.GetUserID();
+            this.isEnabled = true;
+        }
+
+        public void ChangeStrokeWidth(int width)
+        {
+            this.strokeWidth = width;
+            Trace.WriteLine("Whiteboard View Model :: Width changed to : " + width);
+            this.UpdateStrokeWidth();
+        }
+
+        public void ChangeBorderBrush(string bcolour)
+        {
+            this.borderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom(bcolour));
+            Trace.WriteLine("Whiteboard View Model :: border colour changed to : " + bcolour);
+            this.UpdateBorderBrush();
+        }
+
+        public void ChangeFillBrush(string fcolour)
+        {
+            this.fillBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom(fcolour));
+            Trace.WriteLine("Whiteboard View Model :: fill colour changed to : " + fcolour);
+            this.UpdateFillBrush();
+        }
+
+        public void UpdateStrokeWidth()
+        {
+            for(int i = 0; i < selectedShapes.Count(); i++)
+            {
+                ShapeItem newShape = this.selectedShapes[i].DeepClone();
+                newShape.strokeThickness = this.strokeWidth;
+                this.selectedShapes[i] = newShape;
+            }
+        }
+
+        public void UpdateBorderBrush(string bcolour)
+        {
+            for(int i = 0; i < selectedShapes.Count(); i++)
+            {
+                ShapeItem newShape = this.selectedShapes[i].DeepClone();
+                newShape.Stroke = this.borderBrush;
+                this.selectedShapes[i] = newShape;
+            }
+        }
+
+        public void UpdateFillBrush(string fcolour)
+        {
+            for(int i = 0; i < selectedShapes.Count(); i++)
+            {
+                ShapeItem newShape = this.selectedShapes[i].DeepClone();
+                newShape.Fill = this.fillBrush;
+                this.selectedShapes[i] = newShape;
+            }
+        }
     }
 }

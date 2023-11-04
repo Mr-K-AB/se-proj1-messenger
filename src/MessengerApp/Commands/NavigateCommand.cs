9c444f80f56de6daf12c2a3a5822918e9700ce11
Geddam Gowtham <112001011@smail.iitpkd.ac.in>
2023-11-05 00:20:06 +0530

    Added ViewModels and Commands

---
+++ src/MessengerApp/Commands/NavigateCommand.cs
@@ -0,0 +1,24 @@
+﻿using MessengerApp.Services;
+using MessengerApp.Stores;
+using MessengerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessengerApp.Commands
+{
+    public class NavigateCommand : CommandBase
+    {
+        private readonly INavigationService _navigationService;
+
+        public NavigateCommand(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationService.Navigate();
+        }
+    }
+}

34ae9a45d2e97e23370884f42852b5d5eb3fdd0d
siddhu-pikachu <112001034@smail.iitpkd.ac.in>
2023-11-17 23:16:59 -0800

    writing mock classes

---
+++ src/MessengerTests/ContentTests/MockContentNotificationHandler.cs
@@ -0,0 +1,65 @@
+﻿/******************************************************************************
+ * Filename    = MockContentNotificationHandler.cs
+ *
+ * Author      = Rapeti siddhu neehal
+ *
+ * Product     = Messenger
+ * 
+ * Project     = MessengerTests
+ *
+ * Description = Mock Notification handler that handles received data from server   
+ *****************************************************************************/
+
+using MessengerContent.Client;
+using MessengerContent.DataModels;
+
+namespace MessengerTests.ContentTests
+{
+    public class MockContentNotificationHandler : ContentClientNotificationHandler
+    {
+        // member varialbes to store received message and all messages
+        private ChatData _contentData;
+        private List<ChatThread> _chatThreads;
+
+        /// <summary>
+        /// constructor that calls base class ContentClient constructor
+        /// </summary>
+        /// <param name="contentClient"></param>
+        public MockContentNotificationHandler(IContentClient contentClient) : base(contentClient)
+        {
+            _contentData = new ChatData();
+            _chatThreads = new List<ChatThread>();
+        }
+
+        /// <summary>
+        /// Reset member variables
+        /// </summary>
+        private void Reset()
+        {
+            _contentData = new ChatData();
+            _chatThreads = new List<ChatThread>();
+        }
+
+        /// <summary>
+        /// Gets the received message 
+        /// </summary>
+        /// <returns>Object of ContentData class</returns>
+        public ChatData GetReceivedMessage()
+        {
+            Reset();
+            _contentData = _receivedMessage;
+            return _contentData;
+        }
+
+        /// <summary>
+        /// Gets list of chat threads containing all messages
+        /// </summary>
+        /// <returns>List of chat threads</returns>
+        public List<ChatThread> GetAllMessages()
+        {
+            Reset();
+            _chatThreads = _allMessages;
+            return _chatThreads;
+        }
+    }
+}

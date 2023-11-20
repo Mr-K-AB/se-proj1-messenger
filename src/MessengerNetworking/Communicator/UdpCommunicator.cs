0186d634843d067320695600a66460beccfb69b6
priyanshu87511 <110776345+priyanshu87511@users.noreply.github.com>
2023-11-20 13:12:23 +0530

    migrated to tcp

--- src/MessengerNetworking/Communicator/UdpCommunicator.cs
+++ src/MessengerNetworking/Communicator/UdpCommunicator.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿/*
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
@@ -535,3 +536,4 @@ namespace MessengerNetworking.Communicator
         }
     }
 }
+*/

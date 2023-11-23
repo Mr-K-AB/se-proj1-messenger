3096ed7d661ed67ffe71b9669c8c4960a41a5ab6
aadityaraj7769 <ad8051707907@gmail.com>
2023-11-23 23:22:03 +0530

    SharedClientScreenTest

--- src/MessengerTests/ScreenshareTests/SharedClientScreenTest.cs
+++ src/MessengerTests/ScreenshareTests/SharedClientScreenTest.cs
@@ -30,29 +30,6 @@ namespace MessengerTests.ScreenshareTests
     public class SharedClientScreenTest
     {
         /// <summary>
-        /// Update time values after the timeout time.
-        /// </summary>
-        /*
-        public static IEnumerable<object[]> PostTimeoutTime =>
-            new List<object[]>
-            {
-                new object[] { SharedClientScreen.Timeout + 3000 },
-                new object[] { SharedClientScreen.Timeout + 4000 },
-                new object[] { SharedClientScreen.Timeout + 6000 },
-            };
-
-        /// <summary>
-        /// Update time values before the timeout time.
-        /// </summary>
-        public static IEnumerable<object[]> PreTimeoutTime =>
-            new List<object[]>
-            {
-                new object[] { SharedClientScreen.Timeout - 2000 },
-                new object[] { SharedClientScreen.Timeout - 1000 },
-                new object[] { SharedClientScreen.Timeout - 100 },
-            };
-        */
-        /// <summary>
         /// This test is for checking whether client
         /// is successfully disposed or not.
         /// </summary>
